daa7f89534c7c651bbf8d09ee9b83ccb096d0c94
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-01-26 15:45:23 +0200

    Backend: Added smaple data to the backend

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,48 @@ USE `actix_todo`;
 
 DROP TABLE IF EXISTS `subtask`;
 DROP TABLE IF EXISTS `todo`;
-DROP TABLE IF EXISTS `users`;
+DROP DATABASE actix_todo;
+CREATE DATABASE actix_todo;
+USE actix_todo;
 
+-- BASIC SCHEMA
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`email` VARCHAR(256) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS flag (
+CREATE TABLE flag (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(256) NOT NULL UNIQUE,
     `answer` VARCHAR(256) NOT NULL,
+    `description` VARCHAR(32765) NULL,
     `filepath` VARCHAR(256) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS flag_user_mapping (
+CREATE TABLE score (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`flag_id` BIGINT UNSIGNED NOT NULL,
     `user_id` BIGINT UNSIGNED NOT NULL,
     FOREIGN KEY (`flag_id`) REFERENCES `users`(`id`),
     FOREIGN KEY (`user_id`) REFERENCES `flag`(`id`)
 );
+
+
+-- SAMPLE DATA
+INSERT INTO flag(name, answer, filepath, description )
+VALUES
+    ("uzdevums 1", "correct", "task1.zip", "Apraksts nr 1"),
+    ("uzdevums 3", "corrects", "task3.zip", "Apraksts nr 3 loti gars"),
+    ("uzdevums 2", "asdf", "task2.zip", "Apraksts nr 2");
+
+INSERT INTO users (email)
+VALUES
+    ("akmens@asd.lv"),
+    ("ivans@asd.lv");
+
+INSERT INTO score(flag_id, user_id)
+VALUES
+    (1, 1),
+    (2, 1),
+    (2, 2);
