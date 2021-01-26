4771ed7a0d29e2ab31393a19ca5f1466e8ebe0f3
Kristofers Volkovs <kristofers.volkovs@hwswservices.com>
2021-01-26 20:42:34 +0200

    Navbar and Footer are basic design components

--- frontend/src/components/Base/Navbar.tsx
+++ frontend/src/components/Base/Navbar.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement } from 'react';
 
+import style from './Base.module.scss';
+
 export function Navbar(): ReactElement {
   return (
-    <div>
-      Navbar
-    </div>
+    <div className={`${style['navbar-wrapper']}`} />
   );
 }
